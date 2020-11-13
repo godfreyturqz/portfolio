37b6acd6e75633bace9c3391060ce7f86a52d73c
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-13 18:38:24 +0800

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            Copyright 2020 All rights reserved.
+        </div>
+    )
+}
+
+export default Footer
+
