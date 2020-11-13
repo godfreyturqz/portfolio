cdbbd4fda65886a8658473ea59219350e78623f3
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-13 18:39:35 +0800

    minor update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Contact from './components/Contact';
 import DisplayPhoto from './components/DisplayPhoto';
+import Footer from './components/Footer';
 import Header from "./components/Header";
 import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app">
-      <DisplayPhoto className="dp"/>
+      <DisplayPhoto className="photo"/>
       <Header className="header"/>
       <Contact className="contact"/>
       <Projects className="projects"/>
+      <Footer className="footer"/>
     </div>
   );
 }
--- src/components/Header/data.js
+++ src/components/Header/data.js
@@ -2,5 +2,5 @@ export const headerData = {
     heading: 'Hi! I\'m Godfrey Turqueza',
     first: 'I\'m a self-taught programmer and a life-long learner. I\'m an Electrical Engineer by profession, practicing it for almost 5 years but found my passion in web development.',
     second: 'My current skill set are the following:',
-    third: 'HTML, CSS, Bootstrap, JavaScript, jQuery, ReactJS, Redux, NodeJS, Express, MongoDB, PHP, Laravel, SQL, Vue'
+    third: 'HTML, CSS, Bootstrap, JavaScript, jQuery, ReactJS, Redux, NodeJS, Express, MongoDB, PHP, Laravel, SQL, Laravel, Vue'
 }
