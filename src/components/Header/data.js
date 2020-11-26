99408ec5be601b4eca8bc9bc576cefb939cf9332
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-26 15:15:48 +0800

    edit header content

--- src/components/Header/data.js
+++ src/components/Header/data.js
@@ -2,5 +2,5 @@ export const headerData = {
     heading: 'Hi! I\'m Godfrey Turqueza',
     first: 'Self-taught Developer | Electrical Engineer',
     second: 'I\'m a licensed Electrical Engineer in the Philippines and I\'m currently looking for new opportunities in Software Development. Having an engineering background, one of my strengths are solving complex math, logic and algorithm problems.',
-    third: ' My current skill set are the following: HTML, CSS, Bootstrap, JavaScript, jQuery, React, Redux, Node, Express, MongoDB, PHP, SQL. I\'m also familiar Laravel framework.'
+    third: ' My current skill set are the following: HTML, CSS, Bootstrap, JavaScript, jQuery, React, Redux, Node, Express, MongoDB, PHP, SQL. I\'m also familiar with PHP-Laravel framework.'
 }
