bb2f276def4ca435a4728dff794b2ff8e11c8002
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-29 20:44:57 +0800

    Update data.js

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -10,7 +10,7 @@ export const projectData = [
         title: 'E-Commerce',
         description: 'An online bike shop where you can select and buy products. Add it to cart, place an order and pay through Paypal. The goal of this project is to imitate e-commerce platform like Amazon.',
         image: imageOne,
-        techStack: 'ReactJS, NodeJS, Express, MongoDB, Redux, Axios',
+        techStack: 'React, Node, Express, MongoDB, Redux, Redux-Thunk, React-router-dom, Axios, Cloudinary, Bcrypt, JSON Web Token, Mongoose, Paypal REST SDK',
         link: 'https://premium-bikes.herokuapp.com/'
     },
     {
