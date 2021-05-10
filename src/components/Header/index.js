ccb465c0bc61f91b16e1e6eeafaa8eaf84683544
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-05-10 21:13:13 +0800

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,12 +11,12 @@ function Header() {
                     <div className="photo">
                         <img src={imageUrl} alt="me" loading="lazy"/>
                     </div>
-                    <h3>Electrical Engineer | Web Developer</h3>
+                    <h3>Electrical Engineer | Software Developer</h3>
                 </div>
                 <h1>Hi! I'm Godfrey Turqueza</h1>
-                <p>I'm an Electrical Engineer and a WebDev hobbyist. Interested in software development, web development, automation and other engineering related discipline.</p>
+                <p>I'm an Electrical Engineer and a Dev hobbyist. Interested in software development, machine learning, blockchain, automation and engineering.</p>
                 <br/>
-                <p>My main programming language is JavaScript but I'm also familiar with PHP and Python.</p>
+                <p>My main programming language is JavaScript/ TypeScript. Also familiar with PHP and Python.</p>
                 <br/>
             </div>
         </Container>
