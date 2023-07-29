cfbab764182fe9cbeb2d122b9d92a08f335fdb4d
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2023-07-29 13:36:09 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Header from "./components/Header"
+import Header from './components/Header'
 import Projects from './components/Projects'
 import Navbar from './components/Navbar'
 import Tools from './components/Tools'
 
-
 function App() {
-  const isMaintenance = false
+	const isMaintenance = false
 
-  return (
-    isMaintenance ? <div style={{"color":"#fff"}}>Site under maintenance</div> :
-    <>
-      <Navbar/>
-      <div className="main">
-        <Header/>
-        <Projects/>
-        <Contact/>
-        <Tools/>
-      </div>
-      <Footer className="footer"/>
-    </>
-  )
+	return isMaintenance ? (
+		<div style={{ color: '#fff' }}>Site under maintenance</div>
+	) : (
+		<>
+			<Navbar />
+			<div className="main">
+				<Header />
+				<Projects />
+				{/* <Contact/> */}
+				<Tools />
+			</div>
+			<Footer className="footer" />
+		</>
+	)
 }
 
-export default App;
+export default App
